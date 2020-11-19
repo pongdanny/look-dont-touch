0b3e07fab09d4432e20ec7d38b9e4a24af6bf6f5
pongdanny <pongdaniel17@gmail.com>
2020-11-18 20:28:34 -0500

    phase 5 complete

--- src/GalleryView.js
+++ src/GalleryView.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const GalleryView = () => {
-    return (
-        <h2>Gallery View</h2>
-    )
-}
+  const params = useParams();
+
+  const [gallery, setGallery] = useState([]);
+  useEffect(() => {
+    const fetchGallery = async () => {
+      const galleryRes = await fetch(
+        `https://data.nhm.ac.uk/api/3/action/package_show?id=${params.galleryId}`
+      );
+
+      const galleryJSON = await galleryRes.json();
+      setGallery(galleryJSON.result);
+    };
+    fetchGallery();
+  }, [params]);
+
+  return (
+    <>
+      <h2>{gallery.title}</h2>
+      <span>
+        {gallery.isopen ? "Gallery is Open!" : "Gallery is Closed :("}
+      </span>
+      {gallery.resources &&
+        gallery.resources.map((obj) => <GalleryObject key={obj.id} {...obj} />)}
+    </>
+  );
+};
+
+export default GalleryView;
