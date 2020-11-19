7e0d8382d1c2d7ce1cce2adc9c8732f076cc0e52
pongdanny <pongdaniel17@gmail.com>
2020-11-18 20:25:08 -0500

    gallery view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import galleryApi from "./galleryApi";
 import GalleryNavigation from "./GalleryNavigation";
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import GalleryView from "./GalleryView";
 
 function App() {
   const [galleries, setGalleries] = useState([]);
@@ -19,12 +20,12 @@ function App() {
   return (
     <BrowserRouter>
       <GalleryNavigation galleries={galleries}></GalleryNavigation>
-    <Switch>
-      <Route exact path='/'/>
-      <Route path='/gallery/:id' render={()=> <GalleryView />} />
-    </Switch>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Galleries</h1>} />
+        <Route path="/gallery/:id" render={() => <GalleryView />} />
+      </Switch>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
